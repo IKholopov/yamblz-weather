36e5a84df825d9bf789761a7be146476eead1fb9
moggot <9.tilov@gmail.com>
2017-07-09 09:52:16 +0300

    added fragments

---
+++ app/src/main/java/com/example/toor/yamblzweather/presentation/fragment/WeatherFragment.java
@@ -0,0 +1,22 @@
+package com.example.toor.yamblzweather.presentation.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.toor.yamblzweather.R;
+
+public class WeatherFragment extends Fragment {
+
+    public static WeatherFragment newInstance() {
+        return new WeatherFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
