34d52633fb297bec70ca17ed81f26d050cec256f
moggot <9.tilov@gmail.com>
2017-07-18 20:22:13 +0300

    fix bux with fragment title while changing fragment

--- app/src/main/java/com/example/toor/yamblzweather/presentation/mvp/view/fragment/common/BaseFragment.java
+++ app/src/main/java/com/example/toor/yamblzweather/presentation/mvp/view/fragment/common/BaseFragment.java
@@ -1,23 +1,16 @@
 package com.example.toor.yamblzweather.presentation.mvp.view.fragment.common;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public abstract class BaseFragment extends Fragment {
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        setTitle();
-    }
-
-    @Override
     public void onActivityCreated(Bundle saveInstanceState) {
         super.onActivityCreated(saveInstanceState);
 
         setDrawableEnabled();
+        setTitle();
     }
 
     protected abstract void setTitle();
